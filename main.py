c030f026765bd7faab2a3a41ef13ed69c467a2ea
DongJin Shin <dongjin.shin.00@gmail.com>
2020-06-18 07:47:25 +0900

    feat: optimize message

--- main.py
+++ main.py
@@ -9,21 +9,18 @@ def get_timeline(account_type, since_id=None):
     auth.set_access_token(secrets.KEYS[account_type]['ACCESS_TOKEN'], secrets.KEYS[account_type]['ACCESS_TOKEN_SECRET'])
 
     api = tweepy.API(auth)
-    public_tweets = api.home_timeline(since_id=since_id, count=1500//140)
+    public_tweets = api.home_timeline(since_id=since_id, count=100)
 
-    max_id = None
     messages = []
     for tweet in public_tweets:
-        message = '{}[{}]: {}'.format(tweet.user.name, tweet.user.screen_name, tweet.text)
+        message = '{}: {}'.format(tweet.user.name, tweet.text)
         messages.append(message)
-        if max_id is None or tweet.id > max_id:
-            max_id = tweet.id
 
     resp = requests.post('http://localhost:7890/api/letter', data={
         'title': '트위터 {}'.format(account_type),
-        'content': '\n'.join(messages),
+        'content': '|'.join(messages),
         'sender': 'tweet-bot',
-        'isPublic': True,
+        'isPublic': False,
     })
     print(resp)
     try:
